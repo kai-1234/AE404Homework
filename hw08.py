eb0b0928392171ebe14e22e45b7ae2839030a331
kai-1234 <74052281+kai-1234@users.noreply.github.com>
2021-08-27 19:03:30 +0800

    Add files via upload

--- hw08.py
+++ hw08.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 from bs4 import BeautifulSoup
-import requests
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 chrome = webdriver.Chrome()
-poo = []
 #res = requests.get("https://pic.sogou.com/napi/pc/searchList?mode=1&start=48&xml_len=48&query=puppy")
 chrome.get("https://pic.sogou.com/")
 time.sleep(0.5)
@@ -14,34 +13,38 @@ time.sleep(0.5)
 a.send_keys(Keys.ENTER)
 time.sleep(0.5)
 #soup = BeautifulSoup(res.text,'html.parser')
-c = chrome.get('https://pic.sogou.com/pics?query=puppy')
+chrome_options = Options()
 
-divs = soup.find_all('div',class_='img-layout')
-for index,each_div in enumerate(divs):
-    a = each_div.find("a")
-    if not a:
-        continue
-    i = index+1
+#瀏覽器頁面不可視 (背景執行)
+chrome_options.add_argument('--headless')
+
+chrome = webdriver.Chrome(options=chrome_options)
+
+#開啟瀏覽器視窗(chrome)
+#chrome = webdriver.Chrome()
+
+#前往網頁
+chrome.get("https://pic.sogou.com/pics?query=puppy")
+
+#讓程式休息三秒 網頁會延遲一下 確保資料有載入
+time.sleep(3)
+
+#讓游標往下移
+for i in range(20):
+    chrome.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+    time.sleep(1)
+      
+time.sleep(5)
+
+#page_source 讀取網頁原始碼
+soup = BeautifulSoup(chrome.page_source,"html.parser")
+
+j=0
+for div in soup.find_all('div',class_ ='row sc-hjRWVT gvezjq'):
+    j=j+1
+    Name = div.find('h4').text
+    Price = div.find('span',class_='sc-eIHaNI bvDXCA').text
+    print(j,Name,Price)
     
-    poo.append(str(i))
-    img = a.find('img')
-    src = img['src']
-    url = requests.get(src)
-    print(src)
-    
-    with open(str(i)+".png",'wb') as f:
-        f.write(url.content)
-    if index == 199:
-        break
-# chrome.find_element_by_partial_link_text("兒童青少年程式").click()
-# time.sleep(0.5)
-# chrome.maximize_window()
-# time.sleep(0.5)
-# chrome.save_screenshot("wk3afd.png")
-# time.sleep(0.5)
-chrome.close()
-
-
-
-
-
+#關閉網頁    
+#chrome.close()
